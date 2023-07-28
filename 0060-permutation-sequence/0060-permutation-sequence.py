b7a2190fbfe10a182f5bb157b5a5657f6cfc4500
Bagus Dwi Santoso <77262301+bagusdwsn@users.noreply.github.com>
2023-07-28 14:40:36 +0700

    Time: 15 ms (83.81%), Space: 13.2 MB (69.78%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -5,30 +5,29 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        def factorial(num):
+        def get_factorial(num):
             # Calculate the factorial of a number
             if num == 0 or num == 1:
                 return 1
-            return num * factorial(num - 1)
+            return num * get_factorial(num - 1)
 
-        # Create a list of numbers from 1 to n
-        numbers = list(range(1, n + 1))
+        k -= 1  # Convert k to 0-indexed
 
-        # Initialize the result string
+        used = [False] * n
         result = ""
 
-        # Convert k to 0-indexed
-        k -= 1
-
-        # Iterate through the numbers from n to 1
         for i in range(n, 0, -1):
-            # Calculate the index of the number to use from the numbers list
-            index = k // factorial(i - 1)
-            # Append the selected number to the result
-            result += str(numbers[index])
-            # Remove the selected number from the numbers list
-            numbers.pop(index)
-            # Update k to find the next number
-            k %= factorial(i - 1)
+            fact = get_factorial(i - 1)
+            index = k // fact
+            k %= fact
+
+            count = 0
+            for j in range(n):
+                if not used[j]:
+                    count += 1
+                    if count == index + 1:
+                        result += str(j + 1)
+                        used[j] = True
+                        break
 
         return result
