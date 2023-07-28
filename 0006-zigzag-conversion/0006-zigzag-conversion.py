3ecd54253299060b935ac3b47eb5c25e86489fc2
Bagus Dwi Santoso <77262301+bagusdwsn@users.noreply.github.com>
2023-07-28 14:13:45 +0700

    Time: 27 ms (98.4%), Space: 13.4 MB (95.9%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [''] * numRows
+        index, step = 0, 1
+
+        for char in s:
+            rows[index] += char
+
+            # Update the direction of traversal
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+
+            index += step
+
+        return ''.join(rows)
