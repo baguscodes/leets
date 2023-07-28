72b2dddc998e08d3a44bf407c9ad6c363372e2c9
Bagus Dwi Santoso <77262301+bagusdwsn@users.noreply.github.com>
2023-07-28 14:19:11 +0700

    Time: 31 ms (69.34%), Space: 13.4 MB (75.37%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -5,23 +5,17 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        memo = {}  # Memoization dictionary to store already computed results
-        
-        def dp(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
-            
-            if j == len(p):
-                ans = i == len(s)
-            else:
-                first_match = i < len(s) and (s[i] == p[j] or p[j] == '.')
-                
-                if j + 1 < len(p) and p[j + 1] == '*':
-                    ans = (dp(i, j + 2) or (first_match and dp(i + 1, j)))
+        m, n = len(s), len(p)
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+
+        dp[m][n] = True  # Base case: empty string and empty pattern match
+
+        for i in range(m, -1, -1):
+            for j in range(n - 1, -1, -1):
+                first_match = i < m and (s[i] == p[j] or p[j] == '.')
+                if j + 1 < n and p[j + 1] == '*':
+                    dp[i][j] = dp[i][j + 2] or (first_match and dp[i + 1][j])
                 else:
-                    ans = first_match and dp(i + 1, j + 1)
-            
-            memo[(i, j)] = ans
-            return ans
-        
-        return dp(0, 0)
+                    dp[i][j] = first_match and dp[i + 1][j + 1]
+
+        return dp[0][0]
